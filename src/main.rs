0cbd45972b0de62c4d4db81397c9ca1226de89be
Abdurrahman Abdulaziz <abdelrahman_omar_1515@yahoo.com>
2024-04-14 13:11:31 +0200

    unique ID generate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail, Context};
 use core::panic;
 use serde::{Deserialize, Serialize};
 use std::{
@@ -36,30 +36,54 @@ enum Payload {
     EchoOk {
         echo: String,
     },
-}
-
-impl Payload {
-    fn reply(self) -> Payload {
-        match self {
-            Payload::Echo { echo } => Payload::EchoOk { echo },
-            Payload::EchoOk { .. } => panic!("Received echo_ok"),
-            Payload::Init { .. } => Payload::InitOk,
-            Payload::InitOk { .. } => panic!("Received init_ok"),
-        }
-    }
+    Generate,
+    GenerateOk {
+        id: String,
+    },
 }
 
 impl Message {
-    fn handle(self) -> Self {
+    fn handle(self, node_id: &Option<String>) -> Self {
         Message {
             src: self.dest,
             dest: self.src,
             body: Body {
                 msg_id: self.body.msg_id.map(|id| id + 1),
                 in_reply_to: self.body.msg_id,
-                payload: self.body.payload.reply(),
+                payload: match self.body.payload {
+                    Payload::Echo { echo } => Payload::EchoOk { echo },
+                    Payload::EchoOk { .. } => panic!("Received echo_ok"),
+                    Payload::Init { .. } => Payload::InitOk,
+                    Payload::InitOk => panic!("Received init_ok"),
+                    Payload::Generate => {
+                        let node_id = node_id
+                            .as_ref()
+                            .expect("node_id should be set when calling generate");
+                        let msg_id: usize = self
+                            .body
+                            .msg_id
+                            .expect("msg_id should be available in generate message");
+                        Payload::GenerateOk {
+                            id: format!("{node_id} - {}", msg_id),
+                        }
+                    }
+                    Payload::GenerateOk { .. } => panic!("Received echo_ok"),
+                },
             },
         }
+    }
+}
+
+struct Node {
+    id: Option<String>,
+}
+
+impl Node {
+    fn handle(&mut self, msg: Message) -> Message {
+        if let Payload::Init { node_id, .. } = &msg.body.payload {
+            self.id = Some(node_id.clone())
+        }
+        msg.handle(&self.id)
     }
 }
 
@@ -68,9 +92,12 @@ fn main() -> anyhow::Result<()> {
     let mut stdout = std::io::stdout().lock();
 
     let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let mut node = Node { id: None };
+
     for input in inputs {
         let input = input.context("Maelstrom input from STDIN can't be deserialized")?;
-        serde_json::to_writer(&mut stdout, &input.handle()).context("Write to stdout")?;
+        serde_json::to_writer(&mut stdout, &node.handle(input)).context("Write to stdout")?;
         write!(stdout, "\n")?;
     }
 
